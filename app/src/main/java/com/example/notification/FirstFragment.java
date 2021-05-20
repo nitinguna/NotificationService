5788bb9000b068ce2ccf23a808fdf4023bcd0208
nitinguna <njgx68@zebra.com>
2021-05-20 18:05:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notification/FirstFragment.java
@@ -0,0 +1,100 @@
+package com.example.notification;
+
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class FirstFragment extends Fragment {
+
+    private static final String ENABLED_NOTIFICATION_LISTENERS = "enabled_notification_listeners";
+    private static final String ACTION_NOTIFICATION_LISTENER_SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
+    private AlertDialog enableNotificationListenerAlertDialog;
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // If the user did not turn the notification listener service on we prompt him to do so
+
+                if(!isNotificationServiceEnabled()){
+                    enableNotificationListenerAlertDialog = buildNotificationServiceAlertDialog();
+                    enableNotificationListenerAlertDialog.show();
+                }
+                if(isNotificationServiceEnabled()) {
+                    startService();
+                }
+            }
+        });
+    }
+
+    public void startService() {
+        Intent serviceIntent = new Intent(getContext(), ForegroundService.class);
+        serviceIntent.putExtra("inputExtra", "Foreground Service Example in Android");
+        getContext().startService(serviceIntent);
+    }
+
+    private boolean isNotificationServiceEnabled(){
+        String pkgName = getContext().getPackageName();
+        final String flat = Settings.Secure.getString(getContext().getContentResolver(),
+                ENABLED_NOTIFICATION_LISTENERS);
+        if (!TextUtils.isEmpty(flat)) {
+            final String[] names = flat.split(":");
+            for (int i = 0; i < names.length; i++) {
+                final ComponentName cn = ComponentName.unflattenFromString(names[i]);
+                if (cn != null) {
+                    if (TextUtils.equals(pkgName, cn.getPackageName())) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Build Notification Listener Alert Dialog.
+     * Builds the alert dialog that pops up if the user has not turned
+     * the Notification Listener Service on yet.
+     * @return An alert dialog which leads to the notification enabling screen
+     */
+    private AlertDialog buildNotificationServiceAlertDialog(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+        alertDialogBuilder.setTitle(R.string.notification_listener_service);
+        alertDialogBuilder.setMessage(R.string.notification_listener_service_explanation);
+        alertDialogBuilder.setPositiveButton(R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        startActivity(new Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS));
+                    }
+                });
+        alertDialogBuilder.setNegativeButton(R.string.no,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // If you choose to not enable the notification listener
+                        // the app. will not work as expected
+                    }
+                });
+        return(alertDialogBuilder.create());
+    }
+}
---
+++ app/src/main/java/com/example/notification/ForegroundService.java
@@ -0,0 +1,122 @@
+package com.example.notification;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+public class ForegroundService extends NotificationListenerService {
+
+    private static final String TAG = "ForeGroundService";
+    private static final String CHANNEL_ID = "ForegroundServiceChannel";
+
+
+    public ForegroundService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return super.onBind(intent);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        // String input = intent.getStringExtra("inputExtra");
+        createNotificationChannel();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, notificationIntent, 0);
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Foreground Service")
+                .setContentText("Zebra Notification Service")
+                .setContentIntent(pendingIntent)
+                .build();
+        startForeground(1, notification);
+
+        return START_NOT_STICKY;
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(serviceChannel);
+        }
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn){
+         matchNotificationCode(sbn);
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn){
+
+           StatusBarNotification[] activeNotifications = this.getActiveNotifications();
+
+            if(activeNotifications != null && activeNotifications.length > 0) {
+                Log.e(TAG, "Stacked Notification" );
+                for (int i = 0; i < activeNotifications.length; i++) {
+                    Bundle extras = activeNotifications[i].getNotification().extras;
+                    String notificationTitle = extras.getString(Notification.EXTRA_TITLE);
+                    CharSequence notificationText = extras.getCharSequence(Notification.EXTRA_TEXT);
+                    CharSequence notificationSubText = extras.getCharSequence(Notification.EXTRA_SUB_TEXT);
+
+                    if (null != notificationTitle)
+                        Log.e(TAG, notificationTitle );
+
+                    if (null != notificationText)
+                        Log.e(TAG, notificationText.toString() );
+
+                    if (null != notificationSubText)
+                    Log.e(TAG, notificationSubText.toString() );
+                }
+            }
+        }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void matchNotificationCode(StatusBarNotification sbn) {
+        String packageName = sbn.getPackageName();
+        String chnID= sbn.getNotification().getChannelId();
+        Bundle extras = sbn.getNotification().extras;
+
+        if(chnID.equals("DEVICE_ADMIN_ALERTS"))
+        {
+            if (extras.getCharSequence(Notification.EXTRA_TEXT).toString().equals("Installed by your admin") ||
+                    extras.getCharSequence(Notification.EXTRA_TEXT).toString().equals("Updated by your admin")) {
+                Log.e(TAG, "Removing " + extras.getCharSequence(Notification.EXTRA_TEXT).toString() );
+                this.cancelNotification(sbn.getKey());
+            }
+        }
+        else if (chnID.equals("alerting auto granted permissions")){
+            if (extras.getCharSequence(Notification.EXTRA_TITLE).toString().equals("Location can be accessed")) {
+                Log.e(TAG, "Removing " + extras.getCharSequence(Notification.EXTRA_TEXT).toString() );
+                this.cancelNotification(sbn.getKey());
+            }
+        }
+
+
+    }
+}
